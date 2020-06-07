15ca0a04df298a5970803b2e03de1b437f7f9575
Alexander Junge <alexander.junge@posteo.net>
2020-06-07 18:28:02 +0200

    Implement 3 retries when recording fails, then exit

--- record_chunks.py
+++ record_chunks.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import os
 import subprocess
+import sys
+import time
 
 data_dir = "data"
 
@@ -11,6 +13,16 @@ while True:
     output_file = os.path.join(
         output_dir, start_time.strftime("%Y%m%d-%H-%M-%S") + ".wav"
     )
-    print(f"Started writing to {output_file}.")
-    subprocess.run(f"arecord -D plughw:2 -d 60 -f cd {output_file}", shell=True)
-    print(f"Finished writing to {output_file}.")
+    
+    for attempt in range(3):
+        print(f"Started recording to {output_file} (attempt {attempt}).")
+        try:
+            subprocess.run(f"arecord -D plughw:2 -d 60 -f cd {output_file}", shell=True, check=True)
+        except subprocess.CalledProcessError:
+            time.sleep(60)
+        else:
+            print(f"Finished recording to {output_file}.")
+            break
+    else:
+         print(f"Recording to {output_file} failed. Exiting.")
+         sys.exit(1)
