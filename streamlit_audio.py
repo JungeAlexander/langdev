6d8885b668ba3d3e1d0be1a7315a7def47721f1e
Alexander Junge <alexander.junge@gmail.com>
2020-06-12 21:12:19 +0200

    Towards labelling app

--- streamlit_audio.py
+++ streamlit_audio.py
@@ -13,3 +13,9 @@ audio_file = open(audio_path, "rb")
 audio_bytes = audio_file.read()
 
 st.audio(audio_bytes, format="audio/mp3")
+
+button_labels = ["Irina", "Alex", "Baby", "Noise"]
+sound_label = st.multiselect("Sound label", button_labels)
+
+if st.button('Submit labels'):
+    st.write('You selected:', sound_label)
