f971c8c1fcf9e6ed0d18ac9dc97b36d38a128a9c
Alexander Junge <alexander.junge@posteo.net>
2020-08-01 19:08:21 +0200

    Add recording start and end time

--- record_chunks.py
+++ record_chunks.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 import os
 import subprocess
 import sys
@@ -6,14 +6,27 @@ import time
 
 data_dir = "data"
 
+def start_recording(start_time, end_time, now_time):
+    if start_time < end_time:
+        return start_time <= now_time <= end_time
+    else: # past midnight
+        return now_time >= start_time or now_time <= end_time
+
 while True:
-    start_time = datetime.now()
+    start_time = datetime.datetime.now()
+    recording_start_time = datetime.time(20, 00)
+    recording_end_time = datetime.time(6,00)
     output_dir = os.path.join(data_dir, start_time.strftime("%Y%m%d"))
     os.makedirs(output_dir, exist_ok=True)
     output_file = os.path.join(
         output_dir, start_time.strftime("%Y%m%d-%H-%M-%S") + ".wav"
     )
     recording_duration = 60 * 5
+
+    if not start_recording(recording_start_time, recording_end_time, start_time.time()):
+        print(f"{start_time.time()} is not a time to record.")
+        time.sleep(60 * 10)
+        continue
 
     for attempt in range(3):
         print(f"Started recording to {output_file} (attempt {attempt}).")
